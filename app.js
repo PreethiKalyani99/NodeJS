35e9d62df9a42038e721681b582bdebbd0096550
PreethiKalyani99 <preethikalyani99@gmail.com>
2024-08-28 13:08:24 +0530

    read Header

--- app.js
+++ app.js
@@ -1,16 +1,27 @@
 const net = require("net");
 
 const server = net.createServer((socket) => {
-    socket.on("data", (data) => {
-        const url = data.toString()
-        // console.log(url, "url")
-        if(url.includes("/echo/")){
-            const content = url.split(' ')[1].slice(6);
+    socket.on("data", (request) => {
+
+        const url = request.toString().split(" ")[1]
+        const headers = request.toString().split('\r\n')
+        
+        if(url === '/'){
+            socket.write("HTTP/1.1 200 OK\r\n\r\n")
+        }
+        else if(url.includes("/echo/")){
+            const content = url.split('/echo/')[1];
             socket.write(`HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: ${content.length}\r\n\r\n${content}`);
-        }else{
+        }
+        else if(url.toLowerCase() === '/user-agent'){
+            const userAgent = headers.find(header => header.toLowerCase().startsWith('user-agent'))
+            socket.write(`HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: ${userAgent.length}\r\n\r\n${userAgent}`)
+        }
+        else{
             socket.write("HTTP/1.1 404 Not Found\r\n\r\n");
         }
-  });
+        socket.end();
+    });
 });
 
 server.listen(4221);
