72365405195b6c608c3fba3f43a1ebb51386aa62
PreethiKalyani99 <preethikalyani99@gmail.com>
2024-08-28 21:04:57 +0530

    return a file

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const net = require("net");
+const fs = require("fs")
 
 const server = net.createServer((socket) => {
     socket.on("data", (request) => {
@@ -13,6 +14,16 @@ const server = net.createServer((socket) => {
             const content = url.split('/echo/')[1];
             socket.write(`HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: ${content.length}\r\n\r\n${content}`);
         }
+        else if (url.toLowerCase().startsWith('/files/')) {
+            const filePath = `.${url}.js`
+            if (fs.existsSync(`${filePath}`)) {
+                const content = fs.readFileSync(`${filePath}`).toString()
+                const res = `HTTP/1.1 200 OK\r\nContent-Type: application/octet-stream\r\nContent-Length: ${content.length}\r\n\r\n${content}\r\n`
+                socket.write(res);
+              } else {
+                socket.write("HTTP/1.1 404 Not Found\r\n\r\n");
+              }
+        } 
         else if(url.toLowerCase() === '/user-agent'){
             const userAgent = headers.find(header => header.toLowerCase().startsWith('user-agent'))
             socket.write(`HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: ${userAgent.length}\r\n\r\n${userAgent}`)
